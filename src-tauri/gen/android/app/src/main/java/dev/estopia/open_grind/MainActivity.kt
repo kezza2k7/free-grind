e090d86b334a1e303e75cdbe452661b8058abc34
kezza2k7 <jaygithub@estopia.net>
2026-04-20 18:37:27 +0100

    feat: init for android and Ios

---
+++ src-tauri/gen/android/app/src/main/java/dev/estopia/open_grind/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.estopia.open_grind
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
